43744924647a6563de43d979108e8ea73ef68598
Axis-YuAraki <y_araki@axis-net.co.jp>
2025-08-19 16:56:48 +0900

    Create masterfunction.py

---
+++ lambda/masterfunction.py
@@ -0,0 +1,29 @@
+import json
+import boto3
+
+lambda_client = boto3.client('lambda')
+s3 = boto3.resource('s3')
+filepath = ''
+
+Bucket_name = 'test-shikano'
+ObJeCT_KEY_NAME = 'result.html'
+
+def lambda_handler(event, context):
+
+    event["number"] *= 10
+
+    response = lambda_client.invoke(
+
+        FunctionName='calculateprime',
+        InvocationType='RequestResponse', 
+        Payload=json.dumps(event)
+        
+    )
+    result = json.load(response['Payload'])
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps({
+            'Lambda': result,
+        })
+    }
